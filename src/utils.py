76af51fe9855f77870cc23a5982201dedd6f6729
Varun0157 <varun.edachali@gmail.com>
2024-09-29 18:44:12 +0530

    feat: encoder, decoder, transformer

--- src/utils.py
+++ src/utils.py
@@ -20,7 +20,13 @@ class SelfAttention(nn.Module):
         self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
 
-    def forward(self, values, keys, query, mask):
+    def forward(
+        self,
+        values: torch.Tensor,
+        keys: torch.Tensor,
+        query: torch.Tensor,
+        mask: torch.Tensor | None = None,
+    ):
         N = query.shape[0]
         value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
@@ -28,6 +34,10 @@ class SelfAttention(nn.Module):
         values = values.reshape(N, value_len, self.heads, self.head_dim)
         keys = keys.reshape(N, key_len, self.heads, self.head_dim)
         query = query.reshape(N, query_len, self.heads, self.head_dim)
+
+        values = self.values(values)
+        keys = self.keys(keys)
+        query = self.queries(query)
 
         # einsum is easier than having to perform the calculations and determine the
         #   batch multiplication (torch.bmm) because we have n batches
@@ -55,4 +65,42 @@ class SelfAttention(nn.Module):
         )
 
         out = self.fc_out(out)
+        return out
+
+
+class TransformerBlock(nn.Module):
+    def __init__(
+        self, embed_size: int, heads: int, dropout: float, forward_expansion: int
+    ):
+        super(TransformerBlock, self).__init__()
+
+        self.attention = SelfAttention(embed_size=embed_size, heads=heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        # LayerNorm: normalises the output of the attention block
+        # todo: why LayerNorm and not BatchNorm
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size),
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(
+        self,
+        value: torch.Tensor,
+        key: torch.Tensor,
+        query: torch.Tensor,
+        mask: torch.Tensor | None,
+    ):
+        # todo: refactor
+
+        attention = self.attention(value, key, query, mask)
+        x = self.dropout(self.norm1(attention + query))  # NOTE: residual connection
+
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))  # NOTE: residual connection
+
         return out
